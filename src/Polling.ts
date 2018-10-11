6999deedd14af65917f65fcfa97ce7615c32faf5
Felipe Bergamin <felipebergamin6@gmail.com>
2018-10-11 15:33:55 -0300

    change some observable names to keep pattern

--- src/Polling.ts
+++ src/Polling.ts
@@ -12,8 +12,8 @@ import {
 } from "./utils";
 
 export class Polling {
-  public messages: Observable<Update>;
-  public editedMessages: Observable<Update>;
+  public message: Observable<Update>;
+  public editedMessage: Observable<Update>;
   public channelPost: Observable<Update>;
   public editedChannelPost: Observable<Update>;
   public inlineQuery: Observable<Update>;
@@ -62,8 +62,8 @@ export class Polling {
 
     this.observable = this.createObservable().pipe(share());
 
-    this.messages = createFilteredUpdateObservable(this.updates, "message");
-    this.editedMessages = createFilteredUpdateObservable(this.updates, "edited_message");
+    this.message = createFilteredUpdateObservable(this.updates, "message");
+    this.editedMessage = createFilteredUpdateObservable(this.updates, "edited_message");
     this.channelPost = createFilteredUpdateObservable(this.updates, "channel_post");
     this.editedChannelPost = createFilteredUpdateObservable(this.updates, "edited_channel_post");
     this.inlineQuery = createFilteredUpdateObservable(this.updates, "inline_query");
@@ -72,29 +72,29 @@ export class Polling {
     this.shippingQuery = createFilteredUpdateObservable(this.updates, "shipping_query");
     this.preCheckoutQuery = createFilteredUpdateObservable(this.updates, "pre_checkout_query");
 
-    this.text = createFilteredMessageObservable(this.messages, "text");
-    this.audio = createFilteredMessageObservable(this.messages, "audio");
-    this.document = createFilteredMessageObservable(this.messages, "document");
-    this.game = createFilteredMessageObservable(this.messages, "game");
-    this.photo = createFilteredMessageObservable(this.messages, "photo");
-    this.sticker = createFilteredMessageObservable(this.messages, "sticker");
-    this.video = createFilteredMessageObservable(this.messages, "video");
-    this.voice = createFilteredMessageObservable(this.messages, "voice");
-    this.videoNote = createFilteredMessageObservable(this.messages, "video_note");
-    this.contact = createFilteredMessageObservable(this.messages, "contact");
-    this.location = createFilteredMessageObservable(this.messages, "location");
-    this.venue = createFilteredMessageObservable(this.messages, "venue");
-    this.newChatMembers = createFilteredMessageObservable(this.messages, "new_chat_members");
-    this.leftChatMember = createFilteredMessageObservable(this.messages, "left_chat_member");
-    this.newChatTitle = createFilteredMessageObservable(this.messages, "new_chat_title");
-    this.newChatPhoto = createFilteredMessageObservable(this.messages, "new_chat_photo");
-    this.deleteChatPhoto = createFilteredMessageObservable(this.messages, "delete_chat_photo");
-    this.groupChatCreated = createFilteredMessageObservable(this.messages, "group_chat_created");
-    this.supergroupChatCreated = createFilteredMessageObservable(this.messages, "supergroup_chat_created");
-    this.channelChatCreated = createFilteredMessageObservable(this.messages, "channel_chat_created");
-    this.pinnedMessage = createFilteredMessageObservable(this.messages, "pinned_message");
-    this.invoice = createFilteredMessageObservable(this.messages, "invoice");
-    this.successfulPayment = createFilteredMessageObservable(this.messages, "successful_payment");
+    this.text = createFilteredMessageObservable(this.message, "text");
+    this.audio = createFilteredMessageObservable(this.message, "audio");
+    this.document = createFilteredMessageObservable(this.message, "document");
+    this.game = createFilteredMessageObservable(this.message, "game");
+    this.photo = createFilteredMessageObservable(this.message, "photo");
+    this.sticker = createFilteredMessageObservable(this.message, "sticker");
+    this.video = createFilteredMessageObservable(this.message, "video");
+    this.voice = createFilteredMessageObservable(this.message, "voice");
+    this.videoNote = createFilteredMessageObservable(this.message, "video_note");
+    this.contact = createFilteredMessageObservable(this.message, "contact");
+    this.location = createFilteredMessageObservable(this.message, "location");
+    this.venue = createFilteredMessageObservable(this.message, "venue");
+    this.newChatMembers = createFilteredMessageObservable(this.message, "new_chat_members");
+    this.leftChatMember = createFilteredMessageObservable(this.message, "left_chat_member");
+    this.newChatTitle = createFilteredMessageObservable(this.message, "new_chat_title");
+    this.newChatPhoto = createFilteredMessageObservable(this.message, "new_chat_photo");
+    this.deleteChatPhoto = createFilteredMessageObservable(this.message, "delete_chat_photo");
+    this.groupChatCreated = createFilteredMessageObservable(this.message, "group_chat_created");
+    this.supergroupChatCreated = createFilteredMessageObservable(this.message, "supergroup_chat_created");
+    this.channelChatCreated = createFilteredMessageObservable(this.message, "channel_chat_created");
+    this.pinnedMessage = createFilteredMessageObservable(this.message, "pinned_message");
+    this.invoice = createFilteredMessageObservable(this.message, "invoice");
+    this.successfulPayment = createFilteredMessageObservable(this.message, "successful_payment");
   }
 
   public get updates(): Observable<ExplicitTypedUpdate> {
