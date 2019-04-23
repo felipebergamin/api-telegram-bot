954e1154f202fc39c3d22551f1e743545ddcf75f
Felipe Bergamin <felipebergamin6@gmail.com>
2019-04-23 16:24:47 -0300

    add polling basic test

---
+++ __tests__/polling.spec.ts
@@ -0,0 +1,60 @@
+import { take } from 'rxjs/operators';
+import { Polling } from '../src/Polling';
+
+describe('Polling', () => {
+  const serverResponse = {
+    ok: true,
+    result: [
+      {
+        update_id: 1,
+      },
+      {
+        update_id: 2,
+      },
+    ],
+  };
+
+  const mockBot = {
+    getUpdates: jest.fn().mockResolvedValue(serverResponse),
+  };
+
+  const polling = new Polling(mockBot as any);
+
+  test('should start with status === NEW', () => {
+    expect(polling.status).toBe('NEW');
+  });
+
+  test('isPolling() should return false', () => {
+    expect(polling.isPolling).toBe(false);
+  });
+
+  test('should emit received updates one by one', (done) => {
+
+    polling.updates
+      .pipe(take(2))
+      .subscribe({
+        complete: () => {
+          polling.stopPolling();
+          done();
+        },
+        error: (err) => {
+          polling.stopPolling();
+          done.fail(err);
+        },
+        next: (value) => {
+          try {
+            expect(polling.isPolling).toBe(true);
+            expect(value).toEqual(serverResponse.result.shift());
+          } catch (err) {
+            polling.stopPolling();
+            done.fail(err);
+          }
+        },
+      });
+  });
+
+  test('should have status === STOPPED when stops', () => {
+    expect(polling.isPolling).toBe(false);
+    expect(polling.status).toBe('STOPPED');
+  });
+});
